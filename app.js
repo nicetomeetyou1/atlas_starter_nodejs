f2dd95264af23c1f487ba214cea6f65ce520c69d
nicetomeetyou1 <han@scallop.io>
2024-02-21 16:42:30 +0800

    Add axios and express dependencies, update app.js to include API call

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
+const {Axios} = require("axios");
 dotenv.config();
 
-async function main() {
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/data', async (req, res) => {
   try {
     const uri = process.env.MONGO_URI;
     const client = new MongoClient(uri);
+
     await client.connect();
     console.log("Connected to MongoDB");
 
+    const axios = await new Axios({
+      baseURL: "https://reqres.in",
+    }).get("/api/users?page=1");
+    console.log(axios.data);
+    res.json(axios.data);
   } catch (e) {
     console.error(e);
   }
-}
+})
 
-main()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
